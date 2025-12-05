d28b15dc123943f4cecffe54aecc3cf57384e75d
Abdullah Otmane <oabdullae@gmail.com>
2025-12-05 18:59:54 +0100

    Implement collision detection and random food spawn
     - added collision detection for both walls and snake's body, now it just exits
     the game (to be made fancier later)
     - added random food spawning, and a timeout for it to despawn and respawn
     somewhere else
     - design of snake and food is still ugly and looks like minecraft :(
    ^B01K$

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import pdb
+import random
 
 pygame.init()
 
@@ -25,6 +26,8 @@ CHERRY_RED = (255, 0, 80) # snake color
 SUPERMAN_RED = (229, 35, 46) # superman snake red
 SUPERMAN_BLUE = (4, 114, 179) # superman snake blue
 BG_CLR = (32, 32, 32) # temporary color
+
+TIMEOUT = 8000 # 8 seconds
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT));
 clock = pygame.time.Clock()
@@ -91,7 +94,10 @@ def draw_snake_body_block(block, is_head):
 
 # ---
 
-def update_snake(snake_body, snake_ate, direction):
+def update_snake(snake_body, direction):
+    # is the snake gonna eat
+
+    snake_ate = False
     # delete tail snake_body[-1] if snake didnt eat
     if not snake_ate:
         # delete tail snake_body[-1]
@@ -99,6 +105,8 @@ def update_snake(snake_body, snake_ate, direction):
         snake_body.pop(-1)
     else:
         pass # keep tail if snake ate
+
+    
 
     # add a new block in front of the head if there is no collision
     new_block = None
@@ -134,21 +142,63 @@ def update_snake(snake_body, snake_ate, direction):
                 BOX_SIZE
             )
         
+    # detect wall collisions
+    if new_block.x not in range(TOPRIGHT, TOPRIGHT + (PLAYGROUND_BOXES * BOX_SIZE)):
+        return False # There is a collision
+    if new_block.y not in range(TOPRIGHT, TOPRIGHT + (PLAYGROUND_BOXES * BOX_SIZE)):
+        return False # There is a collision
+
+    # detect body collisions
+    for i in range(len(snake_body)):
+        if snake_body[i].x == new_block.x and snake_body[i].y == new_block.y:
+            return False
+
     snake_body.insert(0, new_block)
     draw_snake_body_block(snake_body[0], True)
     draw_snake_body_block(snake_body[1], False)
+    return True
 
 # ---
 
-def spawn_food():
-    pass
+def draw_food(col, row):
+    # temp draw food
+    egg = pygame.Rect(col + 5, row + 5, 35, 35)
+    pygame.draw.rect(screen, WHITE, egg)
+    return egg
 
+# ---
+
+def spawn_food(free_boxes):
+    random_flat_coords = random.choice(free_boxes)
+    j = random_flat_coords % PLAYGROUND_BOXES
+    i = (random_flat_coords - j) // PLAYGROUND_BOXES
+    food_rect = draw_food(TOPRIGHT + j * BOX_SIZE, TOPRIGHT + i * BOX_SIZE)
+    free_boxes.remove(random_flat_coords)
+    return random_flat_coords, food_rect
+
+# ---
+
+def block_to_flat_coords(block):
+    i = (block.y - TOPRIGHT) // BOX_SIZE
+    j = (block.x - TOPRIGHT) // BOX_SIZE
+    return PLAYGROUND_BOXES * i + j
+
+# ---
+
+def clear_old_food(flat_coords, free_boxes):
+    j = flat_coords % PLAYGROUND_BOXES
+    i = (flat_coords - j) // PLAYGROUND_BOXES
+    cleared = pygame.Rect(TOPRIGHT + j * BOX_SIZE, TOPRIGHT + i * BOX_SIZE, BOX_SIZE, 
+        BOX_SIZE)
+    pygame.draw.rect(screen, BG_CLR, cleared)
+    free_boxes.append(flat_coords)
+    
 # ---
 
 first_body_block = pygame.Rect(
     #TOPRIGHT + BOX_SIZE * ((PLAYGROUND_BOXES-1)//2),
     #TOPRIGHT+ BOX_SIZE *((PLAYGROUND_BOXES-1)//2),
-    TOPRIGHT + BOX_SIZE * 3,
+    TOPRIGHT + BOX_SIZE * 8,
     TOPRIGHT+ BOX_SIZE * 8,
     BOX_SIZE,
     BOX_SIZE
@@ -167,8 +217,16 @@ third_body_block = pygame.Rect(
     BOX_SIZE
 )
 """
+
+# initialize free boxes as the whole grid being free
+free_boxes = []
+for i in range(18*18):
+    free_boxes.append(i)
+
+
+# initialize snake body
 snake_body = [first_body_block]
-for i in range(1, 3):
+for i in range(1, 6):
     body_block = pygame.Rect(
         first_body_block.x + BOX_SIZE * i,
         first_body_block.y,
@@ -179,42 +237,60 @@ for i in range(1, 3):
 # snake_body = [first_body_block, second_body_block, third_body_block]
 snake_direction = "LEFT"
 
-pygame.display.flip()
+for i in range(len(snake_body)):
+    free_boxes.remove(block_to_flat_coords(snake_body[i]))
 
+
+
+# Define the box properties (e.g., a 100x100 square starting at 50, 50)
+size = 810
+playground = pygame.Rect(46, 46, size, size)
+# Draw the box using the Rect object
+pygame.draw.rect(screen, BG_CLR, playground)
+
+food_flat_coords, food_rect = spawn_food(free_boxes)
+food_timeout = 0
+
+pygame.display.flip()
 # MAIN LOOP
 while running:
+    # pdb.set_trace()
     arrow_key_pressed = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            # pdb.set_trace()
             if event.key == pygame.K_UP:
                 if snake_direction != "DOWN":
                     arrow_key_pressed = True
                     snake_direction = "UP"
-                    update_snake(snake_body, False, "UP")
+                    if not update_snake(snake_body, "UP"):
+                        running = False
             if event.key == pygame.K_DOWN:
                 if snake_direction != "UP":
                     arrow_key_pressed = True
                     snake_direction = "DOWN"
-                    update_snake(snake_body, False, "DOWN")
+                    if not update_snake(snake_body, "DOWN"):
+                        running = False
             if event.key == pygame.K_RIGHT:
                 if snake_direction != "LEFT":
                     arrow_key_pressed = True
                     snake_direction = "RIGHT"
-                    update_snake(snake_body, False, "RIGHT")
+                    if not update_snake(snake_body, "RIGHT"):
+                        running = False
             if event.key == pygame.K_LEFT:
                 if snake_direction != "RIGHT":
                     arrow_key_pressed = True
                     snake_direction = "LEFT"
-                    update_snake(snake_body, False, "LEFT")
-        else:
-            # call update with just the snake_direction
-            pass
+                    if not update_snake(snake_body, "LEFT"):
+                        running = False
     # screen.fill(BLACK) # to change later for only deleting the relevant parts
     if not arrow_key_pressed:
-        update_snake(snake_body, False, snake_direction)
+        if not update_snake(snake_body, snake_direction):
+            running = False
+            continue
+        pass
+
 
     draw_border(
         (PADDING, PADDING),
@@ -232,11 +308,6 @@ while running:
     pygame.draw.line(screen, RED, (WIDTH - PADDING, PADDING),
         (WIDTH - PADDING, HEIGHT - PADDING), 1)
     """
-    # Define the box properties (e.g., a 100x100 square starting at 50, 50)
-    size = 810
-    playground = pygame.Rect(46, 46, size, size)
-    # Draw the box using the Rect object
-    pygame.draw.rect(screen, BG_CLR, playground)
     """
     for i in range(18):
         snake = pygame.Rect(TOPRIGHT + i*BOX_SIZE,TOPRIGHT, BOX_SIZE, BOX_SIZE)
@@ -261,11 +332,17 @@ while running:
     pygame.draw.rect(screen, SUPERMAN_BLUE, r) 
     """
 
-    spawn_food()
 
     draw_grid()
     pygame.display.flip() # refresh equivalent
 
-    clock.tick(3)
+    dt = clock.tick(3)
+    food_timeout += dt
+    if food_timeout >= TIMEOUT:
+        food_timeout -= TIMEOUT
+        # respawn food somewhere else
+        clear_old_food(food_flat_coords, free_boxes)
+        food_flat_coords, food_rect = spawn_food(free_boxes)
+
 
 pygame.quit()
